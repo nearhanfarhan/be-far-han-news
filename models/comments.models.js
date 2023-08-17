a86d82566450d13861e033d2b7249197315f6d53
Farhan Chaudhry <Farhan@Farhans-MacBook-Pro-7.local>
2023-08-17 09:49:52 +0100

    removed a console log

--- models/comments.models.js
+++ models/comments.models.js
@@ -19,8 +19,8 @@ exports.insertCommentsByArticle = (article_id, author, body) => {
 };
 
 exports.removeCommentById = (comment_id) => {
-  text = "DELETE FROM comments WHERE comment_id = $1 RETURNING*"
-  params = [comment_id]
+  const text = "DELETE FROM comments WHERE comment_id = $1 RETURNING*"
+  const params = [comment_id]
   return db.query(text, params).then(({rows}) => {
     if (rows.length===0) {
       return Promise.reject({status:404, msg: "Comment not found"})
