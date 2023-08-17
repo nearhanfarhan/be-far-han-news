4dc0b9f4c2659de4b09c5d8d3fbf97c7e8d166b3
Farhan Chaudhry <Farhan@Farhans-MacBook-Pro-7.local>
2023-08-17 14:27:30 +0100

    Changes for task 13 - host application

--- listen.js
+++ listen.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 
-const port = 9090;
-app.listen(port, () => {
-  console.log(`listening on ${port}`);
+const { PORT = 9090} = process.env;
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
 });
