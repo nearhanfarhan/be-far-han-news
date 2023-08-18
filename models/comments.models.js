e077eeb3f36df10d8fd15f1f96c6c754f3956022
Farhan Chaudhry <Farhan@Farhans-MacBook-Pro-7.local>
2023-08-18 11:41:51 +0100

    completed task 17

--- models/comments.models.js
+++ models/comments.models.js
@@ -19,12 +19,32 @@ exports.insertCommentsByArticle = (article_id, author, body) => {
 };
 
 exports.removeCommentById = (comment_id) => {
-  const text = "DELETE FROM comments WHERE comment_id = $1 RETURNING*"
-  const params = [comment_id]
-  return db.query(text, params).then(({rows}) => {
-    if (rows.length===0) {
-      return Promise.reject({status:404, msg: "Comment not found"})
+  const text = "DELETE FROM comments WHERE comment_id = $1 RETURNING*";
+  const params = [comment_id];
+  return db.query(text, params).then(({ rows }) => {
+    if (rows.length === 0) {
+      return Promise.reject({ status: 404, msg: "Comment not found" });
     }
-    return Promise.resolve()
+    return Promise.resolve();
+  });
+};
+
+exports.updateCommentById = (comment_id, inc_votes) => {
+  const text =
+    "UPDATE comments SET votes = votes + $1 WHERE comment_id = $2 RETURNING *;";
+  const params = [inc_votes, comment_id];
+  return db.query(text, params).then(({ rows }) => {
+    return rows[0];
+  });
+};
+
+exports.fetchCommentById = (comment_id) => {
+  const text = "SELECT * FROM comments WHERE comment_id = $1;"
+  const params = [comment_id]
+  return db.query(text,params).then(({rows}) => {
+    if (rows.length ===0) {
+      return Promise.reject({status:404, msg:"Comment not found"})
+    }
+    return rows[0]
   })
 }
