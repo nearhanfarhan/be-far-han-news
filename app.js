df75fcd9e2b9641beae12693f5bef0adfea39a09
Farhan Chaudhry <Farhan@Farhans-MacBook-Pro-7.local>
2023-09-04 10:03:55 +0100

    allowed cross origin resource sharing

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
 const express = require("express");
 
-const { handleCustomErrors, handle400Errors} = require("./errors/errors");
+const { handleCustomErrors, handle400Errors } = require("./errors/errors");
 
 const apiRouter = require("./routes/api-router");
+const cors = require("cors");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.use("/api", apiRouter)
+app.use("/api", apiRouter);
 
 app.use((request, response) => {
   response.status(404).send({ msg: "Not found" });
 });
 
-app.use(handleCustomErrors)
+app.use(handleCustomErrors);
 
-app.use(handle400Errors)
+app.use(handle400Errors);
 
 app.use((err, request, response, next) => {
   console.log(err);
